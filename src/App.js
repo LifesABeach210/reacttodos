654fa2abf723256d99f61b8436242d7b08b158eb
Daniel Rothwell <danielrothwell@Daniels-MacBook-Pro.local>
2022-06-10 16:07:45 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="App"
+    >
+<header className="App-logo">
+    <img src={logo} className="App-logo" alt="logo"></img>
+    <MyInfoComponent/>
+</header>
+</div>
+);
 }
 
-export default App;
+const MyInfoComponent = () => {
+  const [myName, setMyName] = useState("Ginny");
+  const [myFavoriteColor, setMyFavoriteColor] = useState("red");
+  const [favoriteMovies, setFavoriteMovies] = useState([
+    "Avengers 1",
+    "Aveng 3",
+    "Avengers 4",
+  ]);
+  return (
+    <div><h4>Name:</h4>
+    <p>{myName}</p>
+    <input
+    type="text"
+    value={myName}
+    onChange={(e)=>{
+      console.logo(e.target.value)
+      setMyName(e.target.value);
+    }}></input><h4>Favorite Color:</h4>
+    < p style={{backgroundColor:myFavoriteColor}}>{myFavoriteColor}</p>
+    <input
+    type="text"
+    onChange={(e) =>{
+      setMyFavoriteColor(e.target.value);
+    }}>
+    </input>
+    <h4>favoriteMovies</h4>
+    <ul>
+    {favoriteMovies.map((element, index)=> {
+      return <li key={`favorite-movie-${index}`}>{element}</li>}
+    )}
+    </ul>
+    <ul>{favoriteMovies.map(()=>{
+      return(
+        <li key={`favorite-movie-input-${index}`}>
+        <label>Movie {index+1}:</label><input type="text"value={favoriteMovies[index]}onChange={(e)=>{
+          console.log(e.target.value)
+          const updatedMovies= [favoriteMovies[0], favoriteMovies[1],favoriteMovies[2]]
+          updatedMovies[index]=e.target.value
+          setFavoriteMovies(updatedMovies)
+        }}>
+        </input>
+        </li>
+      );
+
+    })}}
+    </ul>
+    </div>
+
+
+//export default App;
+
+
+//import React, { useState } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <MyInfoComponent />
+//       </header>
+//     </div>
+//   );
+// }
+//
+// /*
+//   Rules of Components:
+//     1. PascalCase
+//     2. Return Statement With () and single enclosing html element, usually <div></div>
+//     3. Components can be included in other components like html elements
+// */
+//
+// const MyInfoComponent = () => {
+//   const [myName, setMyName] = useState("Ginny");
+//   const [myFavoriteColor, setMyFavoriteColor] = useState("red");
+//   const [favoriteMovies, setFavoriteMovies] = useState([
+//     "Avengers 1",
+//     "Aveng 3",
+//     "Avengers 4",
+//   ]);
+//
+//   return (
+//     <div>
+//       <h4>Name: </h4>
+//       <p>{myName}</p>
+//       <input
+//         type="text"
+//         value={myName}
+//         onChange={(e) => {
+//           console.log(e.target.value)
+//           setMyName(e.target.value);
+//         }}
+//       ></input>
+//       <h4>Favorite Color: </h4>
+//       <p style={{ backgroundColor: myFavoriteColor }}>{myFavoriteColor}</p>
+//       <input
+//         type="text"
+//         onChange={(e) => {
+//           setMyFavoriteColor(e.target.value);
+//         }}
+//       ></input>
+//       <h4>Favorite Movies: </h4>
+//       <ul>
+//         {favoriteMovies.map((element, index) => {
+//           return <li key={`favorite-movie-${index}`}>{element}</li>;
+//         })}
+//       </ul>
+//       <ul>
+//         {favoriteMovies.map((element, index) => {
+//           return (
+//             <li key={`favorite-movie-input-${index}`}>
+//               <label>Movie {index + 1}: </label>
+//               <input type="text" value={favoriteMovies[index]} onChange={(e)=>{
+//                 console.log(e.target.value)
+//                 const updatedMovies = [favoriteMovies[0], favoriteMovies[1], favoriteMovies[2]]
+//                 updatedMovies[index] = e.target.value
+//                 setFavoriteMovies(updatedMovies)
+//               }}>
+//               </input>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//
+//     </div>
+//   );
+// };
+//
+ export default App;
